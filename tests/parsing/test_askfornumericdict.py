f3bdaaa37be30895039d3506d55f8e2e50470993
Peter Cotton <peter.cotton@microprediction.com>
2023-04-26 12:06:05 -0400

    tests

--- tests/parsing/test_askfornumericdict.py
+++ tests/parsing/test_askfornumericdict.py
@@ -2,14 +2,15 @@
 from gptprobe.goodtogo import good_to_go # Poor man's fixture
 
 if good_to_go():
-    from gptprobe.parsing.parsedictrobustly import parse_dict_robustly
+    print('Good to go')
+    from gptprobe.parsing.askfornumericdict import ask_for_numeric_dict
+    from gptprobe.parsing.parsedictrobustly import dict_equal_or_none, parse_dict_robustly
 
-    def assert_pdr(text,answer):
-        d = parse_dict_robustly(text=text)
-
-
-    def test_pdr_0():
+    def test_ask_0():
         text = """ '{"sydney":1234,"adelaide":123}' """
         answer = {'sydney':1234,'adelaide':123}
-        assert_pdr(text=text,answer=answer)
+        response = ask_for_numeric_dict(text=text)
+        d = parse_dict_robustly(text=response)
+        assert dict_equal_or_none(d, answer)
+
 
