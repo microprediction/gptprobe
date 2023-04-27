874d945654151be6aa99a0fa57e9be7488f5416f
Peter Cotton <peter.cotton@microprediction.com>
2023-04-27 12:40:04 -0400

    docs

---
+++ docs/README.md
@@ -0,0 +1,7 @@
+
+
+See 
+
+- [examples](https://github.com/microprediction/gptprobe/tree/main/examples) 
+- [README](https://github.com/microprediction/gptprobe/blob/main/gptprobe/askfor/README.md) in the ask_for diretory
+- 
--- examples/askfor/ask_for_dict_from_text_soccer.py
+++ examples/askfor/ask_for_dict_from_text_soccer.py
@@ -1,13 +1,12 @@
 if __name__=='__main__':
-    from gptprobe.askfor.dictfromquestion import _prompt_for_dict_from_text
-
+    from gptprobe.private_setenv import NOTHING # Sets environ['OPEN_AI_KEY_0'] ... environ['OPEN_AI_KEY_2']
+    from gptprobe.askfor.textfrompoorlyformatteddicttext import ask_for_text_from_poorly_formatted_dict_text
     messy_dict_text = """ Final score are 
                                       Australia  1 
                                       Brazil     2
                                         bummer """
-    d = _prompt_for_dict_from_text(text = messy_dict_text)
-    print(d)
+    print(ask_for_text_from_poorly_formatted_dict_text(text = messy_dict_text))
 
-
+# Result is  {"Australia": "1", "Brazil": "2"}
 
 
