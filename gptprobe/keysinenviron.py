c8ef86c6cdab22171194c7cb2e29ac36ce96c180
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2023-04-29 10:46:23 -0400

    Update keysinenviron.py

--- gptprobe/keysinenviron.py
+++ gptprobe/keysinenviron.py
@@ -1,6 +1,9 @@
 def keys_in_environ():
-    """ Checks that OPEN_AI_KEY_0 are in environment variables, which is
+    """ Checks that OPEN_AI_KEY_0, OPEN_AI_KEY__1,OPEN_AI_KEY_2 are in environment variables, which is
     a prerequisite to using /openapiwrappers
+    
+    Side effect: If it finds just OPEN_AI_KEY instead, this will be copied to OPEN_AI_KEY_0, OPEN_AI_KEY__1,OPEN_AI_KEY_2
+    
     :return:
     """
     import os
@@ -9,4 +12,15 @@ def keys_in_environ():
     except ImportError:
         pass
     checks  = [ os.environ.get(f'OPEN_AI_KEY_{key_choice}') for key_choice in range(3) ]
-    return all(checks)
+    if all(checks):
+        return True
+    else:
+        single_key = os.environ.get('OPEN_AI_KEY')
+        if single_key:
+            for key_choice in range(3):
+                os.environ[f'OPEN_AI_KEY_{key_choice}] = single_key
+            return True
+        else:
+            return False 
+                 
+                           
