1e26e8689ec705674c0ed0a7fc3d69df39e1357f
Peter Cotton <peter.cotton@microprediction.com>
2023-04-29 11:10:57 -0400

    stuff

--- gptprobe/keysinenviron.py
+++ gptprobe/keysinenviron.py
@@ -1,8 +1,11 @@
-def keys_in_environ():
-    """ Checks that OPEN_AI_KEY_0, OPEN_AI_KEY__1,OPEN_AI_KEY_2 are in environment variables, which is
-    a prerequisite to using /openapiwrappers
+def keys_in_environ()->bool:
+    """
+
+      1. Check that OPEN_AI_KEY_0, OPEN_AI_KEY__1,OPEN_AI_KEY_2 are in environment variables, which is
+                                                         a prerequisite to using /openapiwrappers
     
-    Side effect: If it finds just OPEN_AI_KEY instead, this will be copied to OPEN_AI_KEY_0, OPEN_AI_KEY__1,OPEN_AI_KEY_2
+      2.Side effect: If it finds just OPEN_AI_KEY instead, this will be copied to
+                     OPEN_AI_KEY_0, OPEN_AI_KEY__1,OPEN_AI_KEY_2
     
     :return:
     """
@@ -18,7 +21,7 @@ def keys_in_environ():
         single_key = os.environ.get('OPEN_AI_KEY')
         if single_key:
             for key_choice in range(3):
-                os.environ[f'OPEN_AI_KEY_{key_choice}] = single_key
+                os.environ[f'OPEN_AI_KEY_{key_choice}'] = single_key
             return True
         else:
             return False 
