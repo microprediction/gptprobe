ec9da1ecda35539664696aee7773d5bf075153d6
Peter Cotton <peter.cotton@microprediction.com>
2023-05-01 17:03:25 -0400

    dict example

---
+++ examples/askfor/ask_for_dict_rebooting_ai.py
@@ -0,0 +1,9 @@
+from gptprobe.askfor.dictfromquestion import ask_for_dict
+
+
+if __name__=='__main__':
+    from gptprobe.private_setenv import NOTHING
+    question = """What was the result of the 2018 FIFA World Cup final, as a python dict. Do not include anything
+                  except the dict in your answer. One key/value for each team/score."""
+    d = ask_for_dict(question=question)
+    print(d)
